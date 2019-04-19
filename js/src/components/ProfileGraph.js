0b82f62dd151d345703d5eb8673d97b06d9b8582
Jeff McBride <mcbridejc@gmail.com>
2019-04-18 21:59:46 -0700

    remove dead code

--- js/src/components/ProfileGraph.js
+++ js/src/components/ProfileGraph.js
@@ -59,14 +59,6 @@ class ProfileGraph extends Component {
 
   render() {
     const {classes} = this.props;
-    const simpleLineChartData = {
-      labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
-      series: [
-        [12, 9, 7, 8, 5],
-        [2, 1, 3.5, 7, 3],
-        [1, 3, 4, 5, 6]
-      ]
-    }
 
     const chartOptions = {
       axisX: {
